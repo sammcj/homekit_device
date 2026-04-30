ee66a636fac3edac9159b18b7d37e3d9dbaf8f84
Sam McLeod <sammcj@users.noreply.github.com>
2026-04-30 01:24:21 +0000

    Remove unused STAR_PROJECTOR_DEVICE_TYPE metadata

--- homekit_type.py
+++ homekit_type.py
@@ -4,7 +4,6 @@ from homeassistant.const import UnitOfTemperature
 
 # HomeKit Categories (from HAP-python)
 CATEGORY_KETTLE = 27
-CATEGORY_FAN = 3
 
 # HomeKit Characteristic UUIDs (from HAP-python)
 CHAR_ON = "00000025-0000-1000-8000-0026BB765291"
@@ -68,106 +67,8 @@ KETTLE_DEVICE_TYPE = {
     ],
 }
 
-# HomeKit Service UUIDs for additional services used by other device types
-SERVICE_LIGHTBULB = "00000043-0000-1000-8000-0026BB765291"
-SERVICE_FANV2 = "000000B7-0000-1000-8000-0026BB765291"
-
-# HomeKit Characteristic UUIDs for additional characteristics
-CHAR_BRIGHTNESS = "00000008-0000-1000-8000-0026BB765291"
-CHAR_HUE = "00000013-0000-1000-8000-0026BB765291"
-CHAR_SATURATION = "0000002F-0000-1000-8000-0026BB765291"
-CHAR_ROTATION_SPEED = "00000029-0000-1000-8000-0026BB765291"
-CHAR_ACTIVE = "000000B0-0000-1000-8000-0026BB765291"
-
-STAR_PROJECTOR_DEVICE_TYPE = {
-    "category": CATEGORY_FAN,
-    "services": [
-        {
-            "name": "Master",
-            "service": SERVICE_SWITCH,
-            "primary": True,
-            "chars": [
-                {
-                    "name": "Power State",
-                    "char": CHAR_ON,
-                    "device_class": BinarySensorDeviceClass.POWER,
-                },
-            ],
-        },
-        {
-            "name": "Rotation",
-            "service": SERVICE_FANV2,
-            "linked": True,
-            "chars": [
-                {
-                    "name": "Active",
-                    "char": CHAR_ACTIVE,
-                },
-                {
-                    "name": "Rotation Speed",
-                    "char": CHAR_ROTATION_SPEED,
-                    "min_value": 0,
-                    "max_value": 100,
-                    "step_value": 1,
-                },
-            ],
-        },
-        {
-            "name": "Laser",
-            "service": SERVICE_LIGHTBULB,
-            "linked": True,
-            "chars": [
-                {
-                    "name": "On",
-                    "char": CHAR_ON,
-                },
-                {
-                    "name": "Brightness",
-                    "char": CHAR_BRIGHTNESS,
-                    "min_value": 0,
-                    "max_value": 100,
-                    "step_value": 1,
-                },
-            ],
-        },
-        {
-            "name": "Background",
-            "service": SERVICE_LIGHTBULB,
-            "linked": True,
-            "chars": [
-                {
-                    "name": "On",
-                    "char": CHAR_ON,
-                },
-                {
-                    "name": "Brightness",
-                    "char": CHAR_BRIGHTNESS,
-                    "min_value": 0,
-                    "max_value": 100,
-                    "step_value": 1,
-                },
-                {
-                    "name": "Hue",
-                    "char": CHAR_HUE,
-                    "min_value": 0,
-                    "max_value": 360,
-                    "step_value": 1,
-                },
-                {
-                    "name": "Saturation",
-                    "char": CHAR_SATURATION,
-                    "min_value": 0,
-                    "max_value": 100,
-                    "step_value": 1,
-                },
-            ],
-        },
-    ],
-}
-
 DEVICE_TYPES = {
     "kettle": KETTLE_DEVICE_TYPE,
-    "star_projector": STAR_PROJECTOR_DEVICE_TYPE,
 }
 
 def get_device_type(device_type: str) -> dict:
